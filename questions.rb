a997e2e40302a9704b298dc2fb3d3c4915feaf25
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-08-16 21:06:21 +0100

    More test complete

--- questions.rb
+++ questions.rb
@@ -29,27 +29,33 @@ end
 # [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
 # make sure you don't have the same pairing twice, 
 def every_possible_pairing_of_students(array)
-	array.combination(2).to_a
+	array.combination(2)
 end
 
 # discard the first 3 elements of an array, 
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+	array.drop(3)
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+	array.unshift(element)
 end
 
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
+	array.sort_by { |element| element[-1] }
 end
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+	string?
+
+
 end
 
 # turn a positive integer into a negative integer. A negative integer
@@ -82,20 +88,28 @@ end
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+	array.inject(0) { |sum, number| sum += number }
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+	array * 2
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+	symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+	# the correct numerical average of 50/3 w/o rounding up
+	# array.inject(0.0) { |sum, number| sum += number } / array.count
+	
+	# however, the test wants.....
+	array.inject(0.0) { |sum, number| sum += number }
 end
 
 # get all the elements in an array, up until the first element
@@ -109,6 +123,7 @@ end
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+	Hash[*array.flatten]
 end
 
 # get all the letters used in an array of words and return
@@ -122,6 +137,7 @@ end
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+	hash.invert
 end
 
 # in a hash where the keys and values are all numbers
@@ -133,21 +149,25 @@ end
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
+	string.delete('/[A-Z]/')
 end
 
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
+	float.ceil
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+	float.floor
 end
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
+	# date.strftime("%m/%d/%Y")
 end
 
 # get the domain name *without* the .com part, from an email address
@@ -172,15 +192,18 @@ end
 # get the upper limit of a range. e.g. for the range 1..20, you
 # should return 20
 def get_upper_limit_of(range)
+	range.max
 end
 
 # should return true for a 3 dot range like 1...20, false for a 
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+	range.exclude_end?
 end
 
 # get the square root of a number
 def square_root_of(number)
+	Math.sqrt(number)
 end
 
 # count the number of words in a file
--- questions_spec.rb
+++ questions_spec.rb
@@ -40,17 +40,17 @@ describe 'the Friday test :)' do
 
       expect(sorted).to eq [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
    end
-
-   fspecify 'all_elements_except_first_3' do
+   # done
+   specify 'all_elements_except_first_3' do
       n = all_elements_except_first_3 [1, 2, 3, 4, 5, 6, 7]
       expect(n).to eq [4, 5, 6, 7]
    end
-
+   # done
    specify 'add_element_to_beginning_of_array' do
       n = add_element_to_beginning_of_array [2, 3, 4, 5], 1
       expect(n).to eq [1, 2, 3, 4, 5]
    end
-
+   # done
    specify 'array_sort_by_last_letter_of_word' do
       n = array_sort_by_last_letter_of_word ['sky', 'puma', 'maker']
       expect(n).to eq ['puma', 'maker', 'sky']
@@ -91,19 +91,17 @@ describe 'the Friday test :)' do
       n = longest_word_in_array %w(here is a bunch of words of different lengths)
       expect(n).to eq 'different'
    end
-
+   # done
    specify 'total_of_array' do
       n = total_of_array [1, 3, 5, 6, 2, 8]
       expect(n).to eq 25
    end
-
-   # i thought test file shouslnt really > 100 lines of code?!?
-
+   # done - but check!!??
    specify 'double_array' do
       n = double_array [1, 2, 3]
       expect(n).to eq [1, 2, 3, 1, 2, 3]
    end
-
+   # done
    specify 'turn_symbol_into_string' do
       n = turn_symbol_into_string :foobar
       expect(n).to eq 'foobar'
@@ -118,7 +116,7 @@ describe 'the Friday test :)' do
       n = get_elements_until_greater_than_five [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
       expect(n).to eq [1, 3, 5, 4, 1, 2]
    end
-
+   # done
    specify 'convert_array_to_a_hash' do
       n = convert_array_to_a_hash ['a', 'b', 'c', 'd']
       expect(n).to eq({'a' => 'b', 'c' => 'd'})
@@ -143,12 +141,12 @@ describe 'the Friday test :)' do
       n = remove_capital_letters_from_string 'Hello JohnDoe'
       expect(n).to eq 'ello ohnoe'
    end
-
+   # done
    specify 'round_up_number' do
       n = round_up_number 3.142
       expect(n).to eq 4
    end
-
+   # done
    specify 'round_down_number' do
       n = round_down_number 4.9
       expect(n).to eq 4
@@ -164,63 +162,63 @@ describe 'the Friday test :)' do
       expect(n).to eq 'makersacademy'
    end
 
-  specify 'titleize_a_string' do
-    n = titleize_a_string 'the lion the witch and the wardrobe'
-    expect(n).to eq 'The Lion the Witch and the Wardrobe'
-  end
+   specify 'titleize_a_string' do
+      n = titleize_a_string 'the lion the witch and the wardrobe'
+      expect(n).to eq 'The Lion the Witch and the Wardrobe'
+   end
 
-  specify 'check_a_string_for_special_characters' do
-    a = check_a_string_for_special_characters 'ABCdef123'
-    b = check_a_string_for_special_characters 'ABC@def123!'
+   specify 'check_a_string_for_special_characters' do
+      a = check_a_string_for_special_characters 'ABCdef123'
+      b = check_a_string_for_special_characters 'ABC@def123!'
 
-    expect(a).to be_false
-    expect(b).to be_true
-  end
+      expect(a).to be_false
+      expect(b).to be_true
+   end
+   # done
+   specify 'get_upper_limit_of' do
+      n = get_upper_limit_of 1..20
+      expect(n).to eq 20
+   end
+   # done
+   specify 'is_a_3_dot_range?' do
+      a = is_a_3_dot_range? 1..20
+      b = is_a_3_dot_range? 1...20
 
-  specify 'get_upper_limit_of' do
-    n = get_upper_limit_of 1..20
-    expect(n).to eq 20
-  end
+      expect(a).to be false
+      expect(b).to be true
+   end
+   # done
+   specify 'square_root_of' do
+      a = square_root_of 9
+      b = square_root_of 3
+      expect(a).to eq 3.0
+      expect(b).to eq 1.7320508075688772
+   end
 
-  specify 'is_a_3_dot_range?' do
-    a = is_a_3_dot_range? 1..20
-    b = is_a_3_dot_range? 1...20
+   specify 'word_count_a_file' do
+      n = word_count_a_file 'lorem.txt'
+      expect(n).to eq 70
+   end
 
-    expect(a).to be_false
-    expect(b).to be_true
-  end
+   fspecify 'call_method_from_string' do
+      expect { call_method_from_string('foobar') }.to raise_error(NameError)
+   end
 
-  specify 'square_root_of' do
-    a = square_root_of 9
-    b = square_root_of 3
-    expect(a).to eq 3.0
-    expect(b).to eq 1.7320508075688772
-  end
+   specify 'is_a_2014_bank_holiday?' do
+      a = is_a_2014_bank_holiday?(Time.new(2014, 8, 25))
+      b = is_a_2014_bank_holiday?(Time.new(2014, 8, 26))
 
-  specify 'word_count_a_file' do
-    n = word_count_a_file 'lorem.txt'
-    expect(n).to eq 70
-  end
+      expect(a).to be_true
+      expect(b).to be_false
+   end
 
-  specify 'call_method_from_string' do
-    expect { call_method_from_string('foobar') }.to raise_error(NameError)
-  end
+   specify 'your_birthday_is_on_a_friday_in_the_year' do
+      n = your_birthday_is_on_a_friday_in_the_year(Time.new(2013, 1, 1))
+      expect(n).to eq 2016
+   end
 
-  specify 'is_a_2014_bank_holiday?' do
-    a = is_a_2014_bank_holiday?(Time.new(2014, 8, 25))
-    b = is_a_2014_bank_holiday?(Time.new(2014, 8, 26))
-
-    expect(a).to be_true
-    expect(b).to be_false
-  end
-
-  specify 'your_birthday_is_on_a_friday_in_the_year' do
-    n = your_birthday_is_on_a_friday_in_the_year(Time.new(2013, 1, 1))
-    expect(n).to eq 2016
-  end
-
-  specify 'count_words_of_each_length_in_a_file' do
-    n = count_words_of_each_length_in_a_file('lorem.txt') || []
-    expect(Hash[n.sort]).to eq({1=>1, 2=>5, 3=>7, 4=>12, 5=>14, 6=>4, 7=>8, 8=>6, 9=>6, 10=>2, 11=>2, 12=>3}) 
-  end
+   specify 'count_words_of_each_length_in_a_file' do
+      n = count_words_of_each_length_in_a_file('lorem.txt') || []
+      expect(Hash[n.sort]).to eq({1=>1, 2=>5, 3=>7, 4=>12, 5=>14, 6=>4, 7=>8, 8=>6, 9=>6, 10=>2, 11=>2, 12=>3}) 
+   end
 end
